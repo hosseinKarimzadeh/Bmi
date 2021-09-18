78433ceacc06fda80a0da0b5446a903b4ac5c969
Hossein Karimzadeh <89785108+hosseinKarimzadeh@users.noreply.github.com>
2021-09-18 14:07:23 +0430

    Update app.js

--- app.js
+++ app.js
@@ -14,19 +14,19 @@ function getInputValue() {
         let normal1 = Math.round(((heightVal ** 2) * 18.5) / 10000).toFixed(0);
         let normal2 = Math.round(((heightVal ** 2) * 24.9) / 10000).toFixed(0);
         if (bmi < 18.5) {
-            result.innerHTML = `Oops You are under weight`;
+            result.innerHTML = `Oops You are under weight`
             bmiIndex.innerHTML = `Your body mass index: ${bmi}`;
             normalWeight.innerHTML = `The ideal weight is between: ${normal1}kg - ${normal2}kg`;
         } else if (bmi >= 18.5 && bmi < 24.9) {
-            result.innerHTML = `Congratulations you are normal weight`;
+            result.innerHTML = `Congratulations you are normal weight`
             bmiIndex.innerHTML = `Your body mass index: ${bmi}`
             normalWeight.innerHTML = `The ideal weight is between: ${normal1}kg - ${normal2}kg`;
         } else if (bmi >= 25 && bmi < 29.9) {
-            result.innerHTML = `Oops You are overweight`;
+            result.innerHTML = `Oops You are overweight`
             bmiIndex.innerHTML = `Your body mass index: ${bmi}`
             normalWeight.innerHTML = `The ideal weight is between: ${normal1}kg - ${normal2}kg`;
         } else {
-            result.innerHTML = `Unfortunately, you are obese`;
+            result.innerHTML = `Unfortunately, you are obese`
             bmiIndex.innerHTML = `Your body mass index: ${bmi}`
             normalWeight.innerHTML = `The ideal weight is between: ${normal1}kg - ${normal2}kg`;
         }
